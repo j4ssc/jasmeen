8bd7048b12bec6173a82df7a2e823c3fe161f4ed
jasmeen <jasmeenkhatun337@gmail.com>
2025-07-14 10:46:22 +0545

    Prot Route Completed

--- vite-project/src/components/utils/protectedroute.jsx
+++ vite-project/src/components/utils/protectedroute.jsx
@@ -1,17 +1,16 @@
 import {Navigate} from "react-router-dom";
 
-export default function ProtectedRoute({children}) {
+export default function ProtectedRoute({children, allowedRoles = [] }) {
     const token = localStorage.getItem("token");
 
     const user = JSON.parse(localStorage.getItem("user"));
     console.log(user.email);
     if (!token || !user) {
-        return <Navigate to="/" /> ;
+        return <Navigate to="/" />;
     }
 
-if (allowedRoles.length > 0 && !allowedRoles.includes(UserActivation.role)) {
-    return <Navigate to ="/home" replace />;
-}
-
-return children;
+    if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
+        return <Navigate to="/home" replace />
+    }
+    return children;
 }
