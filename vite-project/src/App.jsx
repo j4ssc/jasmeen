0d81a0678196cbef6abe0e8ba6be94d10b3f2e69
jasmeen <jasmeenkhatun337@gmail.com>
2025-07-14 09:45:03 +0545

    Prot Route Completed

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,14 +4,17 @@ import LoginForm from "./pages/login";
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Dashboard from './pages/dashboard';
-
+import ProtectedRoute from './components/utils/protectedroute';
 function App() {
 
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginForm/>} />
-        <Route path="home" element={<Home />} >
+        <Route path="home" element={
+          <ProtectedRoute>
+            <Home />
+            </ProtectedRoute>} >
         <Route path="employee" element={<EmployeeForm />} />
         <Route path="dashboard" element={<Dashboard/>} />
         </Route>
